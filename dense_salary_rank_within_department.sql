f5250a1127daa4570356ed2da31500e8674626ce
RISHABH MISHRA <143255867+0Rishabh@users.noreply.github.com>
2026-05-23 22:11:51 +0530

    Added query to show dense salary rank within each department

---
+++ dense_salary_rank_within_department.sql
@@ -0,0 +1,42 @@
+
+
+
+
+-- =========================================
+-- Problem: Show dense salary rank
+-- within each department.
+-- =========================================
+
+-- Create database
+CREATE DATABASE company_db;
+USE company_db;
+
+-- Create Employees table
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    department VARCHAR(30),
+    salary INT
+);
+
+-- Insert data into employees
+INSERT INTO employees VALUES
+(1, 'Amit', 'IT', 50000),
+(2, 'Riya', 'HR', 40000),
+(3, 'Rahul', 'IT', 70000),
+(4, 'Sneha', 'Finance', 55000),
+(5, 'Ankit', 'HR', 45000),
+(6, 'Pooja', 'IT', 80000),
+(7, 'Karan', 'Support', 30000);
+
+
+-- Solution using WINDOW FUNCTION (DENSE_RANK)
+SELECT 
+    emp_name,
+    department,
+    salary,
+    DENSE_RANK() OVER (
+        PARTITION BY department
+        ORDER BY salary DESC
+    ) AS dense_salary_rank
+FROM employees;
